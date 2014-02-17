a7e6b39a4c2428be43a352efed1c05a09797ca2b
Ethan Stewart <ethan@atdllc.com>
2014-02-17 18:43:31 -0500

    Started to re-implement a tcp socket, time for testing

--- tcpconnection.cpp
+++ tcpconnection.cpp
@@ -1,77 +1,90 @@
 #include "tcpconnection.h"
 
-//TCPConnection::TCPConnection()
-//{
-//    addr.port = 3499;
-//    addr.a = 10;
-//    addr.b = 34;
-//    addr.c = 99;
-//    addr.d = 5;
-//
-//    runnableState = true;
-//
+TCPConnection::TCPConnection()
+{
+    addr.port = 3499;
+    addr.a = 10;
+    addr.b = 34;
+    addr.c = 99;
+    addr.d = 5;
+
+    runnableState = true;
+
 //    ad.sin_family = AF_INET;
 //    ad.sin_len = (u_char) sizeof(sockaddr_in);
 //    ad.sin_port = htons (addr.port);
 //    ad.sin_addr.s_addr = htonl (addr.addr);
-//
-//    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == ERROR) {
-//        ;;// hur dur... da... itsa... problem...
-//        runnableState = false;
-//
-//        // Schedule a retry
-//        return;
-//    }
-//
-//
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == ERROR) {
+        ;;// hur dur... da... itsa... problem...
+        runnableState = false;
+
+        // Schedule a retry
+        return;
+    }
+
+
 //    int optval = 1;  // Turn on no delay
 //    setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof(optval));
-//
-//    // Set to non-blocking
-//    if(!setNonBlocking(true)){
-//        // Output FAILED setting noblocking
-//        runnableState = false;
-//    }
-//
-//    // Bind to the socket
-//    if(bind(sock, ad, sizeof(sockaddr_in)) == ERROR){
-//        // Schedule for retry. again.
-//    }
-//
-//    // We only need to hear for a max of 5 incoming connections.
-//    // This sets the socket to accpet incoming connections (with max 5)
-//    listen(sock, 5);
-//
-//    //int send(descriptor, const char *buf, buflen, int flags);
-//
-//    //int recv(descriptor, char *buf, buflen, flags);
-//
-//    // Schedule a function to accept incoming connections
-//
-//    // Schedule a listen/send event
-//}
-//
-//TCPConnection::~TCPConnection(){
-//    close(sock);
-//}
-//
-//
-//// Scheduled accepter
-//bool TCPConnection::connect(){
-//    // This IS blocking! need to fix!
-//    sockaddr_in *addr;
-//    int *addrlen;
-//    int descriptor = accept(sock, addr, addrlen);
-//}
-//
-//bool setNonBlocking(bool noBlockEh){
-//    int flags;
-//
-//    if (noBlockEh){ /* Set socket to non-blocking */
-//        if ((flags = fcntl(sock_descriptor, F_GETFL, 0)) < 0) return false;
-//        if (fcntl(socket_descriptor, F_SETFL, flags | O_NONBLOCK) < 0) return false;
-//    } else {        /* Set socket to blocking */
-//        if ((flags = fcntl(sock_descriptor, F_GETFL, 0)) < 0) return false;
-//        if (fcntl(socket_descriptor, F_SETFL, flags & (~O_NONBLOCK)) < 0) return false;
-//    }
-//}
+
+    // Set to non-blocking
+    if(!setNonBlocking(true)){
+        // Output FAILED setting noblocking
+        runnableState = false;
+    }
+
+    // Bind to the socket
+    if(bind(sock, &ad, sizeof(sockaddr)) == ERROR){
+        // Schedule for retry. again.
+    }
+
+    // We only need to hear for a max of 5 incoming connections.
+    // This sets the socket to accpet incoming connections (with max 5)
+    listen(sock, 5);
+
+    //int send(descriptor, const char *buf, buflen, int flags);
+
+    //int recv(descriptor, char *buf, buflen, flags);
+
+    // Schedule a function to accept incoming connections
+
+    // Schedule a listen/send event
+}
+
+TCPConnection::~TCPConnection(){
+    close(sock);
+}
+
+
+// Scheduled accepter
+bool TCPConnection::connect(){
+    // This IS blocking! need to fix!
+    sockaddr *addr;
+    int *addrlen;
+    
+    // accept the connection to fd
+    fd = accept(sock, addr, addrlen);
+    
+    if(fd > 0) return true;
+    return false;
+}
+
+bool TCPConnection::setNonBlocking(bool noBlockEh){
+    int flags;
+
+    if (noBlockEh){ /* Set socket to non-blocking */
+        if ((flags = fcntl(sock, F_GETFL, 0)) < 0) return false;
+        if (fcntl(sock, F_SETFL, flags | O_NONBLOCK) < 0) return false;
+    } else {        /* Set socket to blocking */
+        if ((flags = fcntl(sock, F_GETFL, 0)) < 0) return false;
+        if (fcntl(sock, F_SETFL, flags & (~O_NONBLOCK)) < 0) return false;
+    }
+    return true;
+}
+
+int TCPConnection::send(char *str, int len){
+	if(runnableState){
+	    return ::send(fd, str, len, 0);
+	}
+	return -1;
+}
--- tcpconnection.h
+++ tcpconnection.h
@@ -1,50 +1,74 @@
 #ifndef TCPCONNECTION_H
 #define TCPCONNECTION_H
 
-//#include <types.h>
-//#include <net/mbuf.h>
-////#include <mbuf.h>
-//#include <socket.h>
-//#include <sys/socket.h>
-//#include <netinet/in.h>
-//#include <netinet/ip.h>
-//#include <net/socketvar.h>
-//#include <netinet/in.h>
-//#include <stdio.h>
-//#include <sys/types.h>
-//#include <netdb.h>
-//#include <iostream>
-//#include <stdlib.h>
-//
-//class TCPConnection {
-//public:
-//    struct IPStruct {
-//        union {
-//            unsigned addr: 32;
-//            struct {
-//                unsigned a: 8;
-//                unsigned b: 8;
-//                unsigned c: 8;
-//                unsigned d: 8;
-//            };
-//        };
-//        unsigned short port;
-//    };
-//
-//public:
-//    TCPConnection();
-//    ~TCPConnection();
-//
-//private:
-//    IPStruct addr;
-//    sockaddr_in ad;
-//
-//    int sock;
-//
-//    bool runnableState;
-//
-//    // queue
-//};
+#include <types.h>
+#include <net/mbuf.h>
+//#include <mbuf.h>
+#include <socket.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <net/socketvar.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <iostream>
+#include <stdlib.h>
+
+
+#include <socket.h>
+#include <sockLib.h>
+#include <stdlib.h>
+#include <sysLib.h>
+#include <logLib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/socket.h> /*<winsock.h>*/
+#include <stdio.h>
+#include <string.h>
+#include <ioLib.h>
+#include <ioctl.h>
+#include <netLib.h>
+
+class TCPConnection {
+public:
+    struct IPStruct {
+        union {
+            unsigned addr: 32;
+            struct {
+                unsigned a: 8;
+                unsigned b: 8;
+                unsigned c: 8;
+                unsigned d: 8;
+            };
+        };
+        unsigned short port;
+    };
+
+public:
+    TCPConnection();
+    ~TCPConnection();
+    
+    bool connect();
+    bool setNonBlocking(bool noBlockEh);
+
+    int send(char *str, int len);
+    
+private:
+    IPStruct addr;
+    sockaddr ad;
+
+    int sock;
+    int fd;
+
+    bool runnableState;
+
+    // queue
+};
 
 
 #endif // TCPCONNECTION_H
