b1b96c1dc5bde5aed7b74aaf590646b255addd50
Ethan Stewart <ethan@atdllc.com>
2014-03-04 22:09:09 -0500

    Adding functionallity to send team color to arduino

--- arduino.h
+++ arduino.h
@@ -2,17 +2,27 @@
 #define ARDUINO_H
 
 #include <PWM.h>
+#include <DigitalOutput.h>
 
 class ArduinoControl : public PWM {
 public:
+
+    enum TeamColor {
+        TeamBlue = 0, TeamRed = 1
+    };
+
     ArduinoControl(unsigned int pwmport) : PWM(pwmport){
         SetBounds(20.0, 10.0, 10.0, 10.0, 0.0);
         SetPeriodMultiplier(kPeriodMultiplier_4X);
         SetRaw(m_centerPwm);
         this->SetSpeed(0);
+
+        do = new DigitalOutput(8);
     }
     
-    ~ArduinoControl(){}
+    ~ArduinoControl(){
+        delete do;
+    }
 
     void setModeTeleop(){ this->SetSpeed(.2);}
     void setModeAutonomous(){ this->SetSpeed(.4);}
@@ -26,10 +36,17 @@ public:
     void setFlat(){
         SetRaw(m_centerPwm);
     }
+
+    void setTeamColor(TeamColor color){
+        do->set((bool) color);
+    }
+
     
 private:
     int mode;
     int modeStack;
+
+    DigitalOutput *do;
 };
 
 
